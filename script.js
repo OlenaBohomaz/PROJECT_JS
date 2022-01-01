4e3549819e2d035cb1e772919b15aadc15d3e81c
Olena Bohomaz <0960415635q@gmail.com>
2022-01-01 23:35:06 +0200

    First commit

---
+++ script.js
@@ -0,0 +1,47 @@
+
+//"use strict";
+let number = 5;
+const leftBorderWidht = 1;
+
+number = 10;
+console.log (number);
+
+
+const persone = 'Alex jfvkcchx fhcsxkerxr 233474h';
+const bool = false;
+const obj = {
+  name: 'John',
+  age: 25,
+  isMarried: false
+};
+//console.log(obj.name);
+console.log(obj["isMarried"]);
+let arr = ["kmnx vmn", 45, {}, [], 85];
+console.log(arr[0]);
+//alert('Hello!');
+//const result = confirm("Are you here?");
+//console.log(result);
+//const answer = +prompt("Вам есть 18?", "...");
+//console.log(typeof(answer));
+const answers = [];
+answers[0] = prompt('Name?','');
+answers[1] = prompt('Last name?', '');
+answers[2] = prompt('Age?', '');
+//document.write(answers);
+//console.log(typeof(null));
+const category = 'toys';
+console.log(`https://${category}/5`);
+const user = "Ivan";
+alert (`Hi, ${user}!`);
+console.log('arr'+'- object');
+console.log(4 + +'5');
+let incr = 10,
+    decr = 10;
+    incr++;
+    decr--;
+    console.log (incr);
+    console.log (decr);
+const isChecked = false,
+      isClose != true;
+      console.log (isChecked || !isClose);
+
